3579c9d3af9fce3c4df9258df87ad2b48bf5179e
Brian Lalor <blalor@bravo5.org>
2012-06-09 08:19:45 -0400

    More decoding work

--- README.md
+++ README.md
@@ -3,14 +3,26 @@ furry-dangerzone
 
 Decoding the BRZ's SpeedcamUpdates.spud file
 
-Output for the included `.spud` file, created via the nav interface in my '13 BRZ:
+Output for the included `SpeedcamUpdates_all.spud` file, created via the nav interface in my '13 BRZ:
 
-    header: 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 A9 00 E3 FE 00 00 00 00
-    {'angle': 'bi-direction',
-     'camera_type': 'unknown (C0)',
-     'direction': 62,
-     'flag': 0,
-     'latitude': -71.11029127214725,
-     'longitude': 42.40417854075871,
-     'speed_limit': 40}
-    all done
+    unknown stuff | ind | longitude    | latitude    | mph | camera type            | hdg  | direction        | flg
+    "A9 00 E3 FE" |   0 |  -71.1102913 |  42.4041785 |  24 | speed camera           |  62° | bi direction     |   0
+    "BC 00 1C FE" |   1 | -120.8391270 |  47.0069925 |  55 | speed camera           | 330° | single direction |   0
+    "BC 00 20 FE" |   2 | -119.8125005 |  47.1046144 |  55 | speed camera           | 270° | single direction |   0
+    "BA 00 2E FE" |   3 | -116.3041691 |  46.6374018 |  55 | mobile speed camera    | 353° | single direction |   0
+    "BD 00 42 FE" |   4 | -111.3264927 |  47.2690126 |  69 | built-in speed camera  |  83° | bi direction     |   0
+    "BD 00 6D FE" |   5 | -100.6906284 |  47.2682344 |  55 | average speed camera   | 271° | all directions   |   0
+    "BD 00 85 FE" |   6 |  -94.6026996 |  47.2982027 |  55 | red light camera       |   0° | bi direction     |   0
+    "BC 00 9E FE" |   7 |  -88.2649151 |  47.0351089 |  18 | red light and speed    | 270° | bi direction     |   0
+    "B4 00 EC FE" |   8 |  -68.8258975 |  45.1389467 |   5 | bus lane camera        | 336° | bi direction     |   0
+    "AD 00 1D FE" |   9 | -120.5520634 |  43.4251367 |   5 | railway crossing       |   1° | bi direction     |   0
+    "B0 00 4D FE" |  10 | -108.6715168 |  44.0249979 |   5 | high accident zone     | 326° | bi direction     |   0
+    "B3 00 6A FE" |  11 | -101.3495182 |  44.7502329 |   5 | school zone            | 315° | bi direction     |   0
+    "B1 00 95 FE" |  12 |  -90.6940998 |  44.4207269 |   5 | town entry point       |   0° | bi direction     |   0
+    "AE 00 D0 FE" |  13 |  -75.9553531 |  43.5904733 |   5 | toll booth             |   7° | bi direction     |   0
+    "9F 00 18 FE" |  14 | -121.8901906 |  39.9649965 |   5 | hospital, ambulance    |  14° | bi direction     |   0
+    "A2 00 2B FE" |  15 | -117.0257115 |  40.5721475 |   5 | fire station           | 112° | bi direction     |   0
+    "A0 00 40 FE" |  16 | -111.8880925 |  40.1893274 |   5 | congestion charge zone | 276° | bi direction     |   0
+    "9B 00 58 FE" |  17 | -105.7931675 |  38.9828035 |   5 | dangerous area         | 308° | bi direction     |   0
+
+The unknown stuff could be additional data, or a checksum?
--- dump.py
+++ dump.py
@@ -1,21 +1,63 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 # http://www.gpspassion.com/forumsen/topic.asp?TOPIC_ID=77262
 
 import sys
 import struct
+
 from pprint import pprint
 
 epsilon = 1.1920929e-07
 
+## only the ones found during discovery
+CAMERA_TYPES = {
+    # 0x00 : "fixed",
+    # 0x01 : "mobile",
+    # 0x02 : "section",
+    # 0x03 : "built in (traffic)",
+    # 0x04 : "red light (traffic2)",
+
+    0x44 : "red light camera",
+
+    0xc0 : "speed camera",
+    0xc1 : "mobile speed camera",
+    0xc2 : "built-in speed camera",
+    0xc6 : "railway crossing",
+    0xc7 : "bus lane camera",
+    0xc8 : "high accident zone",
+    0xc9 : "school zone",
+    0xca : "town entry point",
+    0xcb : "red light and speed",
+    0xcc : "toll booth",
+    0xcd : "hospital, ambulance",
+    0xce : "fire station",
+    0xcf : "congestion charge zone",
+
+    0xdf : "dangerous area",
+
+    0xe3 : "average speed camera",
+}
+
+MAX_CAM_LENGTH = max(len(CAMERA_TYPES[x]) for x in CAMERA_TYPES)
+
+CAMERA_DIRECTIONS = {
+    0b00 : "single direction",
+    0b01 : "bi direction",
+    0b10 : "all directions",
+    # 0b11 : "all directions",
+}
+
+MAX_CAM_DIR = max(len(CAMERA_DIRECTIONS[x]) for x in CAMERA_DIRECTIONS)
+
 ifp = open(sys.argv[1])
 
-header = ifp.read(24)
-
-print 'header:', " ".join(['%02X' % ord(b) for b in header])
-
 while True:
-    rec = ifp.read(13)
+    header = ifp.read(16)
+    # print 'header:', " ".join(['%02X' % ord(b) for b in header])
+
+    rec = ifp.read(21)
+    # print 'rec:', " ".join(['%02X' % ord(b) for b in rec])
 
     if not rec:
         print "all done"
@@ -56,21 +98,15 @@ while True:
     ##     01H: Deleted Record, when record is deleted, byte 08~0B will be set to 00.
     ##     02H: Edited Record 
 
-    lon, lat, speed_limit, camera_type, dir_and_angle, flag = struct.unpack('<iiBBhB', rec)
+    unk, index, lon, lat, speed_limit, camera_type, dir_and_angle, flag = struct.unpack('<4siiiBBhB', rec)
 
     lat *= epsilon
     lon *= epsilon
 
-    if camera_type == 0:
-        camera_type = "fixed"
-    elif camera_type == 1:
-        camera_type = "mobile"
-    elif camera_type == 2:
-        camera_type = "section"
-    elif camera_type == 3:
-        camera_type = "built-in (traffic)"
-    elif camera_type == 4:
-        camera_type = "red light (traffic2)"
+    speed_limit *= 0.621371192
+
+    if camera_type in CAMERA_TYPES:
+        camera_type = CAMERA_TYPES[camera_type]
     else:
         camera_type = "unknown (%02X)" % camera_type
 
@@ -79,24 +115,41 @@ while True:
 
     angle = dir_and_angle & 0b11
 
-    if angle == 0b00:
-        angle = "single direction"
-    elif angle == 0b01:
-        angle = "bi-direction"
-    elif angle in (0b11, 0b10):
-        angle = "all direction"
+    if angle in CAMERA_DIRECTIONS:
+        angle = CAMERA_DIRECTIONS[angle]
     else:
-        angle = "unknown"
+        angle = "unknown (%02X)" % angle
 
-    data = {
-        'latitude' : lat,
-        'longitude' : lon,
-        'speed_limit' : speed_limit,
-        'camera_type' : camera_type,
-        'direction' : direction,
-        'angle' : angle,
-        'flag' : flag,
+    elements = [
+        ('unk',         " ".join(['%02X' % ord(b) for b in unk])),
+        ('index',       index),
+        ('lon',         lon),
+        ('lat',         lat),
+        ('speed_limit', speed_limit),
+        ('camera_type', camera_type),
+        ('direction',   direction),
+        ('angle',       angle),
+        ('flag',        flag),
+    ]
+
+    # pprint(elements)
+    # 0,42.4041785,-71.1102913,40,speed camera,62,bi-direction,0,169,65251
+
+    formats = {
+        'index'       : '%3d',
+        'lat'         : '%11.7f',
+        'lon'         : '%12.7f',
+        'speed_limit' : '%3d',
+        'camera_type' : ('%%-%ds' % MAX_CAM_LENGTH),
+        'direction'   : u'%3d°',
+        'angle'       : ('%%-%ds' % MAX_CAM_DIR),
+        'flag'        : '%3d',
+        'unk'        : '"%s"',
     }
 
-    pprint(data)
+
+    format_str = " | ".join([formats[x[0]] for x in elements])
+    # print format_str
+    # print [data[x] for x in elements]
+    print (format_str % tuple([x[1] for x in elements])).encode("utf-8")
 
